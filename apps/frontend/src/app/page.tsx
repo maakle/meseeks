e2709941be09ec4d84671364031b967e61778298
Mathias Klenk <mathias.j.klenk@gmail.com>
2025-06-18 14:10:27 +0200

    Implement basic landing page

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,12 +1,29 @@
-import { auth } from '@clerk/nextjs/server';
-import { redirect } from 'next/navigation';
+import FAQ from "@/components/landing/faq";
+import Features from "@/components/landing/features";
+import Footer from "@/components/landing/footer";
+import Hero from "@/components/landing/hero";
+import Pricing from "@/components/landing/pricing";
+import Testimonial from "@/components/landing/testimonial";
+import { Navbar } from "@/components/navbar";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
   const { userId } = await auth();
 
-  if (!userId) {
-    return redirect('/auth/sign-in');
+  if (userId) {
+    return redirect("/auth/sign-in");
   } else {
-    redirect('/dashboard/overview');
+    return (
+      <div>
+        <Navbar />
+        <Hero />
+        <Features />
+        <FAQ />
+        <Testimonial />
+        <Pricing />
+        <Footer />
+      </div>
+    );
   }
 }
---
+++ apps/frontend/src/components/navbar/navbar.tsx
@@ -0,0 +1,36 @@
+import { Button } from "@/components/ui/button";
+import { Logo } from "./logo";
+import { NavMenu } from "./nav-menu";
+import { NavigationSheet } from "./navigation-sheet";
+import ThemeToggle from "@/components/theme-toggle";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="h-16 bg-background border-b border-accent">
+      <div className="h-full flex items-center justify-between max-w-screen-xl mx-auto px-4 sm:px-6">
+        <Logo />
+
+        {/* Desktop Menu */}
+        <NavMenu className="hidden md:block" />
+
+        <div className="flex items-center gap-3">
+          <ThemeToggle />
+          <Link href="/auth/sign-in" className="hidden sm:inline-flex">
+            <Button variant="outline">Sign In</Button>
+          </Link>
+          <Link href="/auth/sign-up" className="hidden xs:inline-flex">
+            <Button>Get Started</Button>
+          </Link>
+
+          {/* Mobile Menu */}
+          <div className="md:hidden">
+            <NavigationSheet />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
